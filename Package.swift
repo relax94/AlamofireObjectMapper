6abb6d282de48246be1733d76c63c589ca8708c0
Dmytro Pavlenko <basket.pavlenko@gmail.com>
2021-10-05 13:55:43 +0300

    fix(om): fixed object mapper dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.9.1")),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "3.5.3")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "3.5.1")),
     ],
     targets: [
         .target(
