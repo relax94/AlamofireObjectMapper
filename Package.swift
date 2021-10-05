885cde2a954e751aa897b6bb510b9eb0ea5c0a78
Dmytro Pavlenko <basket.pavlenko@gmail.com>
2021-10-05 13:33:33 +0300

    fix(package): add spm support for oldest AOM version

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AlamofireObjectMapper",
+    platforms: [
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3),
+        .macOS(.v10_12),
+    ],
+    products: [
+        .library(
+            name: "AlamofireObjectMapper",
+            targets: ["AlamofireObjectMapper"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.9.1")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.2.0")),
+    ],
+    targets: [
+        .target(
+            name: "AlamofireObjectMapper",
+            dependencies: [
+                "Alamofire",
+                "ObjectMapper"
+            ],
+            path: "AlamofireObjectMapper",
+            exclude: [
+                "AlamofireObjectMapperTest",
+                "Carthage"
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
+let version = Version(6, 2, 0)
